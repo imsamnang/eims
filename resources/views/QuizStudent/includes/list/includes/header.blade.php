8a73f4c73d40f43661be508d1df2c30f3fe7f859
semkeamsan <keamsan.sem@gmail.com>
2020-06-23 20:15:03 +0700

    Update header.blade.php

--- resources/views/QuizStudent/includes/list/includes/header.blade.php
+++ resources/views/QuizStudent/includes/list/includes/header.blade.php
@@ -34,14 +34,14 @@
                 {{Translator::phrase("filter")}}
             </span>
         </a>
-        @if (request("quizId"))
+
         <a href="#" data-toggle="report" class="float-right btn btn-success mb-3" role="button" aria-expanded="false">
             <i class="fas fa-file-export m-0"></i>
             <span class="d-none d-sm-inline">
                 {{Translator::phrase("report")}}
             </span>
         </a>
-        @endif
+
     </div>
 </div>
 
@@ -49,8 +49,7 @@
     <form role="filter" class="needs-validation" method="GET" action="{{request()->url()}}" id="form-datatale-filter"
         enctype="multipart/form-data">
         <div class="row flex-lg-row flex-md-row flex-sm-row-reverse flex-xs-row-reverse">
-            <div class="col-12 collapse mb-3 {{request("quizId") ? "show" : ""}}"
-                id="filter">
+            <div class="col-12 collapse mb-3 {{request("quizId") ? "show" : ""}}" id="filter">
                 <div class="form-row">
                     <div class="col-md-8">
                         <select class="form-control" data-toggle="select" id="quiz" title="Simple select"
