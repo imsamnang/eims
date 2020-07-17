004f76292a12c896d644cacf5308a539159649ae
semkeamsan <keamsan.sem@gmail.com>
2020-07-18 03:02:48 +0700

    update

--- app/Helpers/MetaHelper.php
+++ app/Helpers/MetaHelper.php
@@ -10,6 +10,7 @@ class MetaHelper
     public static function setConfig(array $arrayObject)
     {
         Config::set("app.meta", MetaHelper::all($arrayObject));
+        Config::set("app.meta_html", MetaHelper::html($arrayObject));
     }
 
     /**
@@ -31,16 +32,45 @@ class MetaHelper
      */
     public static function all(array $arrayObject)
     {
-        return [
-            'simple'   => MetaHelper::simple($arrayObject),
-            'twitter'   => MetaHelper::twitter($arrayObject),
-            'graph-data'   => MetaHelper::graphData($arrayObject),
-            'google+'   => MetaHelper::googlePlus($arrayObject),
+        return  [
+            "simple"   => MetaHelper::simple($arrayObject),
+            "twitter"   => MetaHelper::twitter($arrayObject),
+            "graph-data"   => MetaHelper::graphData($arrayObject),
+            "google+"   => MetaHelper::googlePlus($arrayObject),
         ];
     }
 
+    public static function html(array $arrayObject)
+    {
+        $metas =  [
+            "simple"   => MetaHelper::simple($arrayObject),
+            "twitter"   => MetaHelper::twitter($arrayObject),
+            "graph-data"   => MetaHelper::graphData($arrayObject),
+            "google+"   => MetaHelper::googlePlus($arrayObject),
+        ];
 
-     /**
+        $html = "";
+        foreach ($metas as $keys) {
+            for ($i = 0; $i < count($keys); $i++) {
+                $meta = array();
+                foreach ($keys[$i] as $name => $item) {
+                    $meta[] = $name;
+                }
+
+                if (count($meta) == 1) {
+                    $html .= "<meta {$meta[0]}=\"{$keys[$i][$meta[0]]}\"/>\n";
+                } else {
+                    $html .= "<meta {$meta[0]}=\"{$keys[$i][$meta[0]]}\" {$meta[1]}=\"{$keys[$i][$meta[1]]}\"/>\n";
+                }
+            }
+        }
+        return $html;
+    }
+
+
+
+
+    /**
      * @param array $arrayObject
      *
      * author => string
@@ -56,37 +86,39 @@ class MetaHelper
     {
         return [
             [
-                'charset'  => 'utf-8'
+                "charset"  => "utf-8"
             ],
             [
                 "name" => "csrf-token",
                 "content" => csrf_token(),
             ],
             [
-                'http-equiv'  => 'content-type',
-                'content'  => 'text/html;charset=utf-8',
+                "http-equiv"  => "content-type",
+                "content"  => "text/html;charset=utf-8",
             ],
             [
-                'name'  => 'viewport',
-                'content'  => 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                "name"  => "viewport",
+                "content"  => "width=device-width, initial-scale=1, shrink-to-fit=no",
             ],
             [
-                'name'  => 'author',
-                'content'  => $arrayObject["author"],
+                "name"  => "author",
+                "content"  => $arrayObject["author"],
             ],
             [
-                'name'  => 'keywords',
-                'content'  => $arrayObject["keywords"],
+                "name"  => "keywords",
+                "content"  => $arrayObject["keywords"],
             ],
             [
-                'name'  => 'description',
-                'content'  => $arrayObject["description"],
+                "name"  => "description",
+                "content"  => $arrayObject["description"],
             ],
         ];
     }
 
 
-     /**
+
+
+    /**
      * @param array $arrayObject
      *
      * title => string
@@ -105,33 +137,33 @@ class MetaHelper
     {
         return [
             [
-                'name'  => 'twitter:card',
-                'content'  => 'twitter:card',
+                "name"  => "twitter:card",
+                "content"  => "twitter:card",
             ],
             [
-                'name'  => 'twitter:site',
-                'content'  => $arrayObject["link"],
+                "name"  => "twitter:site",
+                "content"  => $arrayObject["link"],
             ],
             [
-                'name'  => 'twitter:title',
-                'content'  => $arrayObject["title"],
+                "name"  => "twitter:title",
+                "content"  => $arrayObject["title"],
             ],
             [
-                'name'  => 'twitter:description',
-                'content'  => $arrayObject["description"],
+                "name"  => "twitter:description",
+                "content"  => $arrayObject["description"],
             ],
             [
-                'name'  => 'twitter:creator',
-                'content'  => $arrayObject["author"],
+                "name"  => "twitter:creator",
+                "content"  => $arrayObject["author"],
             ],
             [
-                'name'  => 'twitter:image',
-                'content'  =>  $arrayObject["image"],
+                "name"  => "twitter:image",
+                "content"  =>  $arrayObject["image"],
             ],
         ];
     }
 
-     /**
+    /**
      * @param array $arrayObject
      *
      * title => string
@@ -150,37 +182,37 @@ class MetaHelper
     {
         return [
             [
-                'property'  => 'fb:app_id',
-                'content'  => '',
+                "property"  => "fb:app_id",
+                "content"  => "",
             ],
             [
-                'property'  => 'og:title',
-                'content'  => $arrayObject["title"],
+                "property"  => "og:title",
+                "content"  => $arrayObject["title"],
             ],
             [
-                'property'  => 'og:type',
-                'content'  => '',
+                "property"  => "og:type",
+                "content"  => "",
             ],
             [
-                'property'  => 'og:url',
-                'content'  => $arrayObject["link"],
+                "property"  => "og:url",
+                "content"  => $arrayObject["link"],
             ],
             [
-                'property'  => 'og:image',
-                'content'  => $arrayObject["image"],
+                "property"  => "og:image",
+                "content"  => $arrayObject["image"],
             ],
             [
-                'property'  => 'og:description',
-                'content'  => $arrayObject["description"],
+                "property"  => "og:description",
+                "content"  => $arrayObject["description"],
             ],
             [
-                'property'  => 'og:site_name',
-                'content'  => $arrayObject["title"],
+                "property"  => "og:site_name",
+                "content"  => $arrayObject["title"],
             ],
         ];
     }
 
-     /**
+    /**
      * @param array $arrayObject
      *
      * title => string
@@ -195,16 +227,16 @@ class MetaHelper
     {
         return [
             [
-                'itemprop'  => 'name',
-                'content'  => $arrayObject["title"],
+                "itemprop"  => "name",
+                "content"  => $arrayObject["title"],
             ],
             [
-                'itemprop'  => 'description',
-                'content'  => $arrayObject["description"],
+                "itemprop"  => "description",
+                "content"  => $arrayObject["description"],
             ],
             [
-                'itemprop'  => 'image',
-                'content'  => $arrayObject["image"],
+                "itemprop"  => "image",
+                "content"  => $arrayObject["image"],
             ],
         ];
     }
